6860a1b5c8481c50903e2467ee19d11b6a54a31d
Sofiane <slarbi10@stuy.edu>
2020-02-06 16:15:07 -0500

    BRUH

--- pingpong/paddle/models.py
+++ pingpong/paddle/models.py
@@ -2,57 +2,76 @@ from django.db import models
 
 # Create your models here.
 class Grade(models.Model):
-    GRADES = (
-        ("FR", "Freshman"),
-        ("SP", "Sophomore"),
-        ("JR", "Junior"),
-        ("SR", "Senior"),
-    )
-    name = models.charField(choices=GRADES)
-    def greater(grade1, grade2):
-        pass
+    class Grades(models.TextChoices):
+        FRESHMAN = 'FR', 'Freshman'
+        SOPHOMORE = 'SO', 'Sophomore'
+        JUNIOR = 'JR', 'Junior'
+        SENIOR = 'SR', 'Senior'
+        GRADUATE = 'GR', 'Graduate'
+    name = models.CharField(choices=Grades.choices, default=Grades.FRESHMAN)
+    def greater(self, grade2, grades=Grades.choices):
+        grades = [grade for grade in grades]
+        return grades.index(self.name) < grades.index(self.name)
+
+class PointsCounter(models.Model):
+    points = models.IntegerField()
+
+class Rotation(models.Model):
+    """A, A1, A2, B, B1, B2"""
+    name = models.CharField(max_length=5)
+
+class Period(models.Model):
+    number = models.IntegerField()
+    rotation = models.ForeignKey("paddle.models.Rotation", on_delete=models.DO_NOTHING)
+
+class Schedule(models.Model):
+    classes = models.ManyToManyField("paddle.models.Section")
+
+class Teacher(models.Model):
+    name = models.CharField(max_length=50)
+
+class Section(models.Model):
+    seats = models.IntegerField()
+    course = models.ForeignKey("paddle.models.Course", on_delete=models.DO_NOTHING)
+    period = models.ManyToManyField("paddle.models.Period", on_delete=models.DO_NOTHING)
 
 class Student(models.Model):
-    schedule = models.ForeignKey("pingpong.paddle.models.Schedule")
-    grade = models.ForeignKey("pingpong.paddle.models.Grade")
-    graduationrequirements = models.ForeignKey("pingpong.paddle.models.GraduationRequirements")
-    reportCard = models.ForeignKey("pingpong.paddle.models.ReportCard")
-    points = models.ForeignKey("pingpong.paddle.models.PointsCounter")
+    schedule = models.ForeignKey("paddle.models.Schedule", on_delete=models.DO_NOTHING)
+    grade = models.ForeignKey("paddle.models.Grade", on_delete=models.DO_NOTHING)
+    graduationrequirements = models.ForeignKey("paddle.models.GraduationRequirements", on_delete=models.DO_NOTHING)
+    reportCard = models.ForeignKey("paddle.models.ReportCard", on_delete=models.DO_NOTHING)
+    classWeights = models.ForeignKey("paddle.models.WeightedClassList", on_delete=models.DO_NOTHING)
+    points = models.ForeignKey("paddle.models.PointsCounter", on_delete=models.DO_NOTHING)
     def hasPassed(self, course):
         pass
     def isTaking(self, course):
-        return course in self.schedule
+        pass
 
 class Course(models.Model):
     course_id = models.CharField(max_length=50)
-    # Related class Period has superclass Course and can relate to it
-    prefs = models.ForeignKey("pingpong.paddle.models.Prefrences")
-    department = models.ForeignKey("pingpong.paddle.models.Department")
-    subject = models.ForeignKey("pingpong.paddle.models.Subject")
-    permissions = models.ForeignKey("pingpong.paddle.models.PermissionsList")
-    reqsFulfilled = models.ForeignKey("pingpong.paddle.models.Requirements")
+    # Related class Section has superclass Course and can relate to it
+    prefs = models.ForeignKey("paddle.models.Prefrences", on_delete=models.DO_NOTHING)
+    department = models.ForeignKey("paddle.models.Department", on_delete=models.DO_NOTHING)
+    subject = models.ForeignKey("paddle.models.Subject", on_delete=models.DO_NOTHING)
+    permissions = models.ForeignKey("paddle.models.PermissionsList", on_delete=models.DO_NOTHING)
+    reqsFulfilled = models.ForeignKey("paddle.models.Requirements", on_delete=models.DO_NOTHING)
     syllabus = models.FileField()
     description = models.TextField()
-    course_group = reqsFulfilled = models.ForeignKey("pingpong.paddle.models.CourseGroup")
+    course_group = reqsFulfilled = models.ForeignKey("paddle.models.CourseGroup", on_delete=models.DO_NOTHING)
     def qualifies(self, student):
         pass
 
 class CourseGroup(models.Model):
     name = models.CharField(max_length=50)
 
-class Section(models.Model):
-    scheduleList = models.ForeignKey("pingpong.paddle.models.Schedule")
-    seats = models.IntegerField()
-    periods = models.ManyToManyField("pingpong.paddle.models.Periods")
-    section = models.CharField(max_length=20)
-    teacher = models.ForeignKey("pingpong.paddle.models.Teacher")
+# create class Operator 
 
 class Permission(models.Model):
     OPERATORS = (("OR", "or"), ("AND", "and"))
-    conditions = models.ManyToManyField("pingpong.paddle.models.Condition")
+    conditions = models.ManyToManyField("paddle.models.Condition", on_delete=models.DO_NOTHING)
     operator = models.CharField(choices=OPERATORS, max_length=5)
 
-class Condition(model.Model):
+class Condition(models.Model):
     name = models.CharField(max_length=20)
     def applyCondition(self, delegate):
         pass
