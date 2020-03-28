91fba4c507e43fe5c7683fd0fc1f7ea2eb2fc812
Sofiane <slarbi10@stuy.edu>
2020-03-28 14:34:10 -0400

    added rl stuff

--- pingpong/paddle/marl.py
+++ pingpong/paddle/marl.py
@@ -4,6 +4,7 @@ from collections import namedtuple
 import gym
 import numpy as np
 from ray.rllib.env.multi_agent_env import MultiAgentEnv
+from .models import Student
 
 class Config():
     """ Model related settings """
@@ -14,35 +15,46 @@ class Config():
     MAX_POINTS = 100
     EMPTY_SPACE_ID = -1
     CLASS_BIN_SIZE = 20
+    subject = Student
 
-    """ RL related settings """
-    STUDENT_OBSERVATION_SPACE = gym.spaces.Dict({
-        "my_classes": gym.spaces.Box(low=EMPTY_SPACE_ID, high=MAX_CLASS_ID, shape=(NUM_PERIODS,)),
-        "available_classes": gym.spaces.Box(low=EMPTY_SPACE_ID, high=MAX_CLASS_ID, shape=(CLASS_BIN_SIZE,))
-    })
+    BUY_OBS_SPACE = gym.spaces.Discrete(2) # (period, points) + current schedule
+    SELL_OBS_SPACE = gym.spaces.Tuple((gym.spaces.Discrete(NUM_PERIODS), gym.spaces.Discrete(1))) # [(period, points) for class in classes)] + [(period, points) of highest bidding]
 
-    STUDENT_ACTION_SPACE = gym.spaces.Dict({
-        "trade_out": gym.spaces.Discrete(NUM_PERIODS-1), # the index of which class to trade out
-        "trade_in": gym.spaces.Box(low=np.array([0,0]), high=np.array([CLASS_BIN_SIZE-1, NUM_PERIODS-1]), shape=(2,)), #  [intaking index, out index] 
-        "do_nothing": gym.spaces.Discrete(2)
-    })
+    BUY_ACTION_SPACE = gym.spaces.Discrete(1) # points
+    SELL_ACTION_SPACE = gym.spaces.Discrete(1) # period 
 
-    BUY_OBS_SPACE = gym.spaces.Discrete(2)
-    SELL_OBS_SPACE = gym.spaces.Tuple((gym.spaces.Discrete(NUM_PERIODS), gym.spaces.Discrete(1)))
-
-    BUY_ACTION_SPACE = gym.spaces.Discrete(1)
-    SELL_ACTION_SPACE = gym.spaces.Discrete(1)
+def choose_env(worker_index, vector_index_):
+    pass
 
 
+class AssignmentEnv(MultiAgentEnv):
+    def __init__(self, env_config):
+        self.env = gym.make(choose_env(env_config.worker_index, env_config.vector_index))
+        self.action_space = self.env.action_space 
+        self.observation_space = self.env.observation_space 
+        
+    def with_agent_groups(self, groups, obs_space=None, act_space=None):
+        pass
+    
+    def reset(self):
+        return self.env.reset() 
+    
+    def step(self, action):
+        return self.env.step(action)
+    
 class BuyEnv(MultiAgentEnv):
     def __init__(self):
         self.action_space = Config.BUY_ACTION_SPACE
         self.observation_space = Config.BUY_OBS_SPACE
-        self.agents = []
 
     def reset(self):
-        for agent in agents:
-            agent.reset 
+        pass # return dict of {agent: obs space,...}
+    
+    def get_subject(self, agent_str):
+        config.subject.objects.get(pk=agent_str[2:])
+    
+    def _obs_space(self):
+        pass
 
     def step(self, action, agent):
         pass
